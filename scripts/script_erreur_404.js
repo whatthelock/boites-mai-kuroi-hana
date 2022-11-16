471e741acfe9eb83a794ac4bf5b03bf12df34543
J-c-Zephir <90851637+J-c-Zephir@users.noreply.github.com>
2022-11-16 09:58:51 -0500

    im in a commit (ed) relationship with gitty (kawai emoji)

---
+++ scripts/script_erreur_404.js
@@ -0,0 +1,15 @@
+"use strict";
+
+document.body.addEventListener("mousemove", evt => {
+  const mouseX = evt.clientX;
+  const mouseY = evt.clientY;
+  gsap.set(".cursor", {
+    x: mouseX,
+    y: mouseY
+  });
+  gsap.to(".shape", {
+    x: mouseX,
+    y: mouseY,
+    stagger: -0.1
+  });
+});
