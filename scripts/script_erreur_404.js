a04dde3a95ead0e58dc7eaf8b72c2d11dddf7012
J-c-Zephir <90851637+J-c-Zephir@users.noreply.github.com>
2022-11-21 19:21:05 -0500

    gsap animation done
    with small bug

--- scripts/script_erreur_404.js
+++ scripts/script_erreur_404.js
@@ -1,35 +1,3 @@
-// gsap.registerPlugin(ScrollTrigger);
-
-// gsap.to('.section__404__title', { 
-//   rotation: 1080,
-//   scrollTrigger: {
-//     scrub: 0.5,
-//     markers: true,
-//     start: 'top 80%',
-//     end: 'bottom 35%',
-//   }
-// })
-
-// const animation = document.querySelector('body');
-
-// animation.addEventListener('click', () => {
-//   gsap.to('.section__404__animation__arrosoir', { 
-//     rotation: '0deg',
-//   });
-
-// gsap.from('#1', { 
-//   rotateX: '90%',
-//   opacity: 0
-// });
-
-// gsap.from('#2', { 
-//   rotateX: '90%',
-//   opacity: 0
-// });
-
-// });
-
-
 gsap.to('.section__404__animation__arrosoir', {
   rotation: '0deg',
 });
@@ -45,15 +13,3 @@ gsap.to('.section__404__animation__goutte', {
   opacity : 0,
   stagger: 0.1
 });
-
-// const animation = gsap.timeline({
-//   defaults: { 
-//     y: '100%',
-//   }
-// })
-
-// gsap.to('.section__404__animation__goutte', { 
-//   delay: 2
-
-// })
-// gsap.to('.section__404__animation__goutte', { delay: 2 });
